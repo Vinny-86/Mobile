d8badba7983de55841896ad2f691acc00ef38768
Vinny-86 <vinisilva.nh@gmail.com>
2023-10-09 09:14:47 -0300

    Teste de Branch

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>App Mobile, Texto de teste de Branch</Text>
+			<Text>Texto de teste de Branch</Text>
 			<StatusBar style='auto' />
 		</View>
 	);
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#f1f1f1',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
